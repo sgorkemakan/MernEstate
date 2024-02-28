2f78ebd54508c5ffcd4019053a7c571c9bf74a49
sgorkemakan <117218846+sgorkemakan@users.noreply.github.com>
2024-02-28 19:52:28 +0300

    Create Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,35 @@
+export default function Footer() {
+  return (
+    <div>
+      <ul className="flex flex-wrap justify-center mt-3 text-lg font-medium text-gray-500 sm:mt-0 pb-3">
+        <li>
+          <a
+            href="mailto:s.gorkemakan@gmail.com"
+            target="_blank"
+            className="hover:underline me-4 md:me-6"
+          >
+            Mail Me
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/SahinGorkemAkan"
+            target="_blank"
+            className="hover:underline me-4 md:me-6"
+          >
+            My Facebook
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/sgorkemakan?tab=repositories"
+            target="_blank"
+            className="hover:underline me-4 md:me-6"
+          >
+            My GitHub
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
